ec0e8716d067e8184b73709dd73038dbd0d4e7aa
Marim99 <marim.khae>
2021-10-25 22:38:22 +0200

    f

--- __test__/testUrl.spec.js
+++ __test__/testUrl.spec.js
@@ -1,13 +1,12 @@
 import { checkURL } from "../src/client/js/checkURL";
+describe("Testing, the checkURL function to be defined", () => {
+  test("checkUrl to be defined", () => {
+    expect(checkURL).toBeDefined();
+  });
+});
 
 describe("Testing, the inputUrl must exsit", () => {
   test("return true", () => {
     expect(checkURL).toBeTruthy();
-  });
-});
-
-describe("Testing, the checkURL function to be defined", () => {
-  test("checkUrl to be defined", () => {
-    expect(checkURL).toBeDefined();
   });
 });
